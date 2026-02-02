eb20bd743d895ce0cd443c1cc1e926de10d5d958
Thomas Stang <thomas.stang@voxmedia.com>
2026-02-01 20:23:21 -0600

    feat(workspace): add workspace module barrel export

---
+++ src/core/workspace/index.ts
@@ -0,0 +1,39 @@
+// Types
+export { type GlobalConfig, GlobalConfigSchema, ProfileNameSchema } from './types';
+
+// Resolver utilities
+export {
+  getWorkspaceRoot,
+  isWorkspaceMode,
+  getProfilesDir,
+  getProfileDir,
+  getProfileConfigPath,
+  getProfileEnvPath,
+  getWorkLogDir,
+  getAnalysisDir,
+  getOutputsDir,
+  getGlobalConfigPath,
+  ensureWorkspaceRoot,
+  ensureProfileDirs,
+  profileExists,
+} from './resolver';
+
+// Global config
+export {
+  loadGlobalConfig,
+  saveGlobalConfig,
+  getActiveProfile,
+  setActiveProfile,
+} from './global-config';
+
+// Profile manager
+export {
+  validateProfileName,
+  listProfiles,
+  createProfile,
+  deleteProfile,
+  loadProfileConfig,
+  saveProfileConfig,
+  saveProfileEnv,
+  initializeWorkspaceWithProfile,
+} from './profile-manager';
